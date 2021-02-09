51d513d7bb62a800405830da6ec9065a099333b8
yonsweng <yonsweng@gmail.com>
2021-02-09 14:31:41 +0900

    REINFORCE not working

--- main.py
+++ main.py
@@ -11,21 +11,20 @@ class Policy(nn.Module):
     """
     def __init__(self):
         super(Policy, self).__init__()
-        self.conv = nn.Conv1d(3, 8, 1)
-        self.linear1 = nn.Linear(8 * 77, 256)
-        self.linear2 = nn.Linear(260, 64)
-        self.linear3 = nn.Linear(64, 4)
+        self.conv = nn.Conv1d(3, 32, 1)
+        self.linear1 = nn.Linear(32 * 77, 2048)
+        self.linear2 = nn.Linear(2048, 1024)
+        self.linear3 = nn.Linear(1024, 4)
         self.saved_log_probs = []
+        self.saved_entropies = []
         self.rewards = []
 
-    def forward(self, x, a):
+    def forward(self, x):
         x = F.relu(self.conv(x))
         x = torch.flatten(x, 1)
         x = F.relu(self.linear1(x))
-        x = torch.cat((x, a), 1)
         x = F.relu(self.linear2(x))
         x = self.linear3(x)
-        x = F.softmax(x, 1)
         return x
 
 
@@ -33,11 +32,10 @@ class Policy(nn.Module):
 ACTION_NAMES = ['NORTH', 'EAST', 'WEST', 'SOUTH']
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 policy = Policy()
-last_a = torch.tensor([[0., 0., 0., 0.]], device=device)
+last_a = -1
 
-if __name__ == '__main__':
-    # load policy
-    policy.load_state_dict(torch.load('submission/policy.pt', map_location=device))
+# load policy
+# policy.load_state_dict(torch.load('submission/policy.pt', map_location=device))
 
 policy.to(device)
 policy.eval()
@@ -46,24 +44,38 @@ policy.eval()
 def preprocess(observation):
     x = torch.zeros(1, 3, 77)
     for food in observation['food']:
-        x[0, 0, food] = 1.0
-    for i, goose in enumerate(observation['geese'], 1):
-        i = 2 if i >= 2 else 1
+        x[0, 0, food] = 1.
+    for i, goose in enumerate(observation['geese']):
         if len(goose) > 0:
-            x[0, i, goose[0]] = 1.0
-        for part in goose[1:]:
-            x[0, i, part] = 0.5
+            i = 1 if i == observation['index'] else 2
+            x[0, i, goose[0]] = 1.
+            for part in goose[1:]:
+                x[0, i, part] = .5
     return x
+
+
+def opposite(a):
+    return 3 - a
 
 
 def agent(observation, configuration, save=False):
     global ACTION_NAMES, device, policy, last_a
     observation = preprocess(observation).to(device)
-    probs = policy(observation, last_a)
+    logits = policy(observation)
+    if last_a != -1:
+        # remove illigal move
+        illigal_move = opposite(last_a)
+        mask = torch.arange(4)
+        mask = mask[mask != illigal_move].to(device)
+        probs = torch.zeros_like(logits, device=device)
+        probs[:, mask] = F.softmax(logits[:, mask], 1)
+    else:
+        probs = F.softmax(logits, 1)
     m = Categorical(probs)
     action = m.sample()
     if save:
         policy.saved_log_probs.append(m.log_prob(action))
-    last_a = F.one_hot(action, 4).float().to(device)
-    action = ACTION_NAMES[action.item()]
+        policy.saved_entropies.append(m.entropy())
+    last_a = action.item()
+    action = ACTION_NAMES[last_a]
     return action
--- train.py
+++ train.py
@@ -8,6 +8,16 @@ from kaggle_environments import make
 from main import policy, agent
 
 
+def process_reward(reward, done):
+    if done:
+        if reward > 0:
+            return 1
+        else:
+            return -1
+    else:
+        return 0
+
+
 def finish_episode():
     R = 0
     policy_loss = []
@@ -16,30 +26,38 @@ def finish_episode():
         R = r + args.gamma * R
         returns.insert(0, R)
     returns = torch.tensor(returns)
+
+    # standardization
     if len(returns) > 1:
         returns = (returns - returns.mean()) / (returns.std() + eps)
+    else:
+        returns = returns - returns.mean()
+
     for log_prob, R in zip(policy.saved_log_probs, returns):
         policy_loss.append(-log_prob * R)
     optimizer.zero_grad()
     policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
+    # entropy_loss = -torch.tensor(policy.saved_entropies).sum()
+    loss = policy_loss  # + entropy_loss
+    loss.backward()
     optimizer.step()
     del policy.rewards[:]
     del policy.saved_log_probs[:]
+    del policy.saved_entropies[:]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='REINFORCE')
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                        help='discount factor (default: 0.99)')
+    parser.add_argument('--gamma', type=float, default=0.98, metavar='G',
+                        help='discount factor (default: 0.98)')
     parser.add_argument('--seed', type=int, default=543, metavar='N',
                         help='random seed (default: 543)')
     parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                         help='interval between status logs (default: 100)')
-    parser.add_argument('--lr', type=float, default=1e-6, metavar='G',
-                        help='learning rate (default: 1e-6)')
-    parser.add_argument('--cuda', type=int, default=0, metavar='G',
-                        help='gpu number (default: 0)')
+    parser.add_argument('--step-log-interval', type=int, default=1000, metavar='N',
+                        help='interval between step logs (default: 1000)')
+    parser.add_argument('--lr', type=float, default=1e-5, metavar='G',
+                        help='learning rate (default: 1e-5)')
     args = parser.parse_args()
 
     env = make('hungry_geese', debug=False)
@@ -49,27 +67,32 @@ if __name__ == '__main__':
     # settings & constants
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    tb = SummaryWriter()
+    tb = SummaryWriter(comment=args.__str__())
     eps = np.finfo(np.float32).eps.item()
 
     # training the agent in the first position
-    trainer = env.train([None, 'random', 'random', 'random'])
+    trainer = env.train([None, 'examples/risk_averse.py', 'examples/risk_averse.py', 'examples/risk_averse.py'])
     policy.train()
     optimizer = optim.Adam(policy.parameters(), lr=args.lr)
 
     # for log
     ep_rewards = deque(maxlen=args.log_interval)
     ep_steps = deque(maxlen=args.log_interval)
+    actions = deque(maxlen=args.step_log_interval)
+    action2int = {'NORTH': 0, 'EAST': 1, 'WEST': 2, 'SOUTH': 3}
 
-    for i_episode in range(1, 500000):
+    for i_episode in range(1, 50001):
         observation, ep_reward = trainer.reset(), 0
 
-        for step in range(100):
+        for step in range(1, 101):
             action = agent(observation, configuration, save=True)
             observation, reward, done, info = trainer.step(action)
-            reward = process_reward(observation, reward, done)
+            reward = process_reward(reward, done)
             policy.rewards.append(reward)
             ep_reward += reward
+
+            actions.append(action2int[action])
+
             if done:
                 break
 
@@ -81,10 +104,11 @@ if __name__ == '__main__':
             avg_reward = sum(ep_rewards) / len(ep_rewards)
             avg_step = sum(ep_steps) / len(ep_steps)
             print(f'Episode {i_episode}\t'
-                  f'Average reward: {avg_reward:.2f}'
-                  f'Average steps: {avg_step:.2f}')
+                  f'Average reward: {avg_reward:.2f}\t'
+                  f'Average steps: {avg_step:.2f}\t')
             tb.add_scalar('reward', avg_reward, i_episode)
             tb.add_scalar('step', avg_step, i_episode)
+            tb.add_histogram('actions', np.array(actions), i_episode)
             torch.save(policy.state_dict(), 'models/policy.pt')
 
     tb.close()
