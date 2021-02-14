6d7160912223fb6381e839006a246177663105fd
yonsweng <yonsweng@gmail.com>
2021-02-15 01:07:32 +0900

    Remove warnings

--- models.py
+++ models.py
@@ -19,8 +19,10 @@ class TorusConv2d(nn.Module):
         self.bn = nn.BatchNorm2d(output_dim) if bn else None
 
     def forward(self, x):
-        h = torch.cat([x[:, :, :, -self.edge_size[1]:], x, x[:, :, :, :self.edge_size[1]]], dim=3)
-        h = torch.cat([h[:, :, -self.edge_size[0]:], h, h[:, :, :self.edge_size[0]]], dim=2)
+        h = torch.cat([x[:, :, :, -self.edge_size[1]:], x,
+                       x[:, :, :, :self.edge_size[1]]], dim=3)
+        h = torch.cat([h[:, :, -self.edge_size[0]:], h,
+                       h[:, :, :self.edge_size[0]]], dim=2)
         h = self.conv(h)
         h = self.bn(h) if self.bn is not None else h
         return h
@@ -31,7 +33,8 @@ class GeeseNet(nn.Module):
         super().__init__()
         layers, filters = 12, 32
         self.conv0 = TorusConv2d(17, filters, (3, 3), True)
-        self.blocks = nn.ModuleList([TorusConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
+        self.blocks = nn.ModuleList([TorusConv2d(filters, filters,
+                                     (3, 3), True) for _ in range(layers)])
         self.head_p = nn.Linear(filters, 4, bias=False)
         self.head_v = nn.Linear(filters * 2, 1, bias=False)
 
