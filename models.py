d9c242c7b382398a75054c32fbd3e5723762ca67
yonsweng <yonsweng@gmail.com>
2021-02-15 00:33:52 +0900

    Update model to TorusConv

--- models.py
+++ models.py
@@ -1,31 +1,51 @@
+'''
+Copied from
+https://www.kaggle.com/yuricat/smart-geese-trained-by-reinforcement-learning
+Modified by
+yonsweng (Choi Yeonung)
+in Feb 2021
+'''
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Value(nn.Module):
-    """
-    Input: observation (1, 10 * 77 + 1)
-    Output: value (1, 1)
-    """
+# Neural Network for Hungry Geese
+class TorusConv2d(nn.Module):
+    def __init__(self, input_dim, output_dim, kernel_size, bn):
+        super().__init__()
+        self.edge_size = (kernel_size[0] // 2, kernel_size[1] // 2)
+        self.conv = nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size)
+        self.bn = nn.BatchNorm2d(output_dim) if bn else None
+
+    def forward(self, x):
+        h = torch.cat([x[:, :, :, -self.edge_size[1]:], x, x[:, :, :, :self.edge_size[1]]], dim=3)
+        h = torch.cat([h[:, :, -self.edge_size[0]:], h, h[:, :, :self.edge_size[0]]], dim=2)
+        h = self.conv(h)
+        h = self.bn(h) if self.bn is not None else h
+        return h
+
+
+class GeeseNet(nn.Module):
     def __init__(self):
-        super(Value, self).__init__()
-        self.linear0 = nn.Linear(10 * 77 + 1, 2048)
-        self.linear1 = nn.Linear(2048, 2048)
-        self.linear2 = nn.Linear(2048, 2048)
-        # self.linear3 = nn.Linear(2048, 2048)
-        # self.linear4 = nn.Linear(2048, 2048)
-        # self.linear5 = nn.Linear(2048, 2048)
-        # self.linear6 = nn.Linear(2048, 2048)
-        self.linear7 = nn.Linear(2048, 1)
+        super().__init__()
+        layers, filters = 12, 32
+        self.conv0 = TorusConv2d(17, filters, (3, 3), True)
+        self.blocks = nn.ModuleList([TorusConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
+        self.head_p = nn.Linear(filters, 4, bias=False)
+        self.head_v = nn.Linear(filters * 2, 1, bias=False)
+
+        self.saved_log_probs = []
+        self.saved_rewards = []
         self.saved_values = []
 
     def forward(self, x):
-        x = F.leaky_relu(self.linear0(x))
-        x = F.leaky_relu(self.linear1(x))
-        x = F.leaky_relu(self.linear2(x))
-        # x = F.leaky_relu(self.linear3(x))
-        # x = F.leaky_relu(self.linear4(x))
-        # x = F.leaky_relu(self.linear5(x))
-        # x = F.leaky_relu(self.linear6(x))
-        x = self.linear7(x)
-        return x
+        h = F.relu_(self.conv0(x))
+        for block in self.blocks:
+            h = F.relu_(h + block(h))
+        h_head = (h * x[:, :1]).view(h.size(0), h.size(1), -1).sum(-1)
+        h_avg = h.view(h.size(0), h.size(1), -1).mean(-1)
+        p = self.head_p(h_head)
+        v = torch.tanh(self.head_v(torch.cat([h_head, h_avg], 1)))
+
+        return {'policy': p, 'value': v}
--- train.py
+++ train.py
@@ -6,105 +6,115 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
 from torch.utils.tensorboard import SummaryWriter
-from collections import deque
-from copy import deepcopy
 from kaggle_environments import make
-from main import Policy, ACTION_NAMES, device, policy, preprocess, agent
-from models import Value
+from models import GeeseNet
 
 
-def process_reward(obs, reward, done):
+'''
+make_input and agent functions
+Copied from
+https://www.kaggle.com/yuricat/smart-geese-trained-by-reinforcement-learning
+Modified by
+yonsweng (Choi Yeonung)
+in Feb 2021
+'''
+obs_prev = None
+
+
+def make_input(obs):
+    b = np.zeros((17, 7 * 11), dtype=np.float32)
+
+    for p, pos_list in enumerate(obs['geese']):
+        # head position
+        for pos in pos_list[:1]:
+            b[0 + (p - obs['index']) % 4, pos] = 1
+        # tip position
+        for pos in pos_list[-1:]:
+            b[4 + (p - obs['index']) % 4, pos] = 1
+        # whole position
+        for pos in pos_list:
+            b[8 + (p - obs['index']) % 4, pos] = 1
+
+    # previous head position
+    if obs_prev is not None:
+        for p, pos_list in enumerate(obs_prev['geese']):
+            for pos in pos_list[:1]:
+                b[12 + (p - obs['index']) % 4, pos] = 1
+
+    # food
+    for pos in obs['food']:
+        b[16, pos] = 1
+
+    return b.reshape(-1, 7, 11)
+
+
+def agent(obs, _, train=False):
+    global obs_prev
+
+    x = make_input(obs)
+    obs_prev = obs
+
+    xt = torch.from_numpy(x).unsqueeze(0).to(device)
+    o = model(xt)
+
+    p = o['policy'].squeeze(0)
+    m = Categorical(logits=p)
+    action = m.sample()
+
+    if train:
+        model.saved_log_probs.append(m.log_prob(action))
+        model.saved_values.append(o['value'].squeeze())
+
+    actions = ['NORTH', 'SOUTH', 'WEST', 'EAST']
+    return actions[action.item()]
+
+
+def process_reward(obs, done):
     '''
-    winning bonus:
-        10  if kill
-        -30 if killed
-    length bonus:
-        5 per length increase
-    step bonus:
-        1 per step
+    reward:
+        +1   if 1st
+        1/3  if 2nd
+        -1/3 if 3rd
+        -1   if 4th
     '''
-    killed, len_diff = 0, 0
-
-    if obs.step == 1:
-        reward -= 101
-
-    if reward <= 0:  # if lose
-        killed = 0
-    else:
-        # calc killed
-        curr_alive = 0
+    if done:
+        alive = 0
         for goose in obs.geese[1:]:
-            if len(goose) > 0:
-                curr_alive += 1
-
-        killed = process_reward.prev_alive - curr_alive
-        process_reward.prev_alive = curr_alive
-
-        if curr_alive > 0 and done:  # if step 199
-            killed = 0
-
-        len_diff = reward - 100
-
-    return 10 * killed + 0 * len_diff + 1
+            if len(goose) >= len(obs.geese[0]):
+                alive += 1
+        return (3 - 2 * alive) / 3
+    return 0.
 
 
-def finish_episode(i_episode):
-    epi_len = len(policy.saved_rewards)
-    sum_reward = 0.
+def finish_episode():
+    epi_len = len(model.saved_rewards)
     td_targets = [0.] * epi_len
+    sum_reward = 0.
     gamma_td = args.gamma ** args.td
     for i in range(epi_len - 1, -1, -1):
         tail = i + args.td + 1
         if tail < epi_len:
-            sum_reward -= gamma_td * policy.saved_rewards[tail]
-        sum_reward = args.gamma * sum_reward + policy.saved_rewards[i]
+            sum_reward -= gamma_td * model.saved_rewards[tail]
+        sum_reward = args.gamma * sum_reward + model.saved_rewards[i]
         td_targets[i] = sum_reward + \
-            (gamma_td * args.gamma * value.saved_values[tail]
-             if tail < epi_len else torch.tensor([[0.]], device=device))
+            (gamma_td * args.gamma * model.saved_values[tail]
+             if tail < epi_len else torch.tensor(0., device=device))
 
-    log_probs = torch.cat(policy.saved_log_probs)
-    td_targets = torch.cat(td_targets).squeeze()
-    values = torch.cat(value.saved_values).squeeze()
+    log_probs = torch.stack(model.saved_log_probs)
+    td_targets = torch.stack(td_targets)
+    values = torch.stack(model.saved_values)
+
     policy_loss = -(log_probs * (td_targets - values).detach()).sum()
-    if finish_episode.entropy_coef > 0:
-        # spread probs
-        policy_loss += -sum(policy.saved_entropies).squeeze() \
-            * finish_episode.entropy_coef
-        finish_episode.entropy_coef -= finish_episode.entropy_coef_reduce
     value_loss = F.smooth_l1_loss(values, td_targets.detach(), reduction='sum')
+    loss = policy_loss + value_loss
 
-    policy_loss.backward()
-    value_loss.backward()
-    if i_episode % args.batch_size == 0:
-        optimizer.step()
-        value_optimizer.step()
-        optimizer.zero_grad()
-        value_optimizer.zero_grad()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
-    del policy.saved_rewards[:]
-    del policy.saved_log_probs[:]
-    del policy.saved_entropies[:]
-    del policy.saved_logits[:]
-    del value.saved_values[:]
-
-
-def opponent(observation, configuration):
-    index = observation.index - 1
-    observation = preprocess(observation, configuration)
-    logits = oppo_policy[index](observation)
-    m = Categorical(logits=logits)
-    if last_a[index] != -1:
-        # remove illigal move
-        illigal_move = 3 - last_a[index]  # opposite
-        probs = F.softmax(logits, 1)
-        probs[:, illigal_move] = 0
-        probs /= probs.sum()
-        action = Categorical(probs).sample()
-    else:
-        action = m.sample()
-    last_a[index] = action.item()
-    action = ACTION_NAMES[last_a[index]]
-    return action
+    del model.saved_log_probs[:]
+    del model.saved_rewards[:]
+    del model.saved_values[:]
 
 
 if __name__ == '__main__':
@@ -115,130 +125,67 @@ if __name__ == '__main__':
                         help='random seed (default: 543)')
     parser.add_argument('--log-interval', type=int, default=500, metavar='N',
                         help='interval between status logs (default: 500)')
-    parser.add_argument('--step-log-interval', type=int, default=1000,
-                        metavar='N',
-                        help='interval between step logs (default: 1000)')
-    parser.add_argument('--change-interval', type=int, default=1000,
-                        metavar='N',
-                        help='interval btw changing opponent (default: 1000)')
-    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
-                        help='# of episodes for batching (default: 1)')
+    parser.add_argument('--n-epi', type=int, default=500000, metavar='N',
+                        help='number of episodes to train (default: 500000)')
     parser.add_argument('--lr', type=float, default=1e-6, metavar='G',
                         help='learning rate (default: 1e-6)')
-    parser.add_argument('--vlr', type=float, default=1e-6, metavar='G',
-                        help='learning rate for value (default: 1e-6)')
-    parser.add_argument('--l2', type=float, default=0, metavar='G',
-                        help='l2 regularization (default: 0)')
     parser.add_argument('--td', type=int, default=0, metavar='N',
                         help='td(n) (default: 0)')
-    parser.add_argument('--prev-pi', type=int, default=15, metavar='N',
-                        help='# of previous policies to save (default: 15)')
-    parser.add_argument('--start-self', type=int, default=1, metavar='N',
-                        help='episode # to start self-play (default: 1)')
-    # for train resume
-    parser.add_argument('--entropy-steps', type=int, default=0,
-                        metavar='N', help='steps for entropy loss (0: off)')
     parser.add_argument('--load', type=str, metavar='S', default='',
-                        help='loading model name')
+                        help='model name to load')
     args = parser.parse_args()
 
+    # environment
     env = make('hungry_geese', debug=False)
-    configuration = env.configuration
-    print(configuration)
+    trainer = env.train([None, agent, agent, agent])
 
     # settings & constants
     random.seed(args.seed)
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    eps = np.finfo(np.float32).eps.item()
-    if args.entropy_steps > 0:
-        finish_episode.entropy_coef = 1
-        finish_episode.entropy_coef_reduce = 1 / args.entropy_steps
-    else:
-        finish_episode.entropy_coef = 0
 
-    # training the agent in the first position
-    trainer = env.train([None,
-                         'examples/simple_bfs.py',
-                         'examples/risk_averse.py',
-                         'examples/mighty_boiler_goose.py'])
-    policy.train()
-    oppo_policy = [Policy().to(device).eval() for _ in range(3)]
-    value = Value().to(device)
-    optimizer = optim.Adam(policy.parameters(), lr=args.lr)
-    value_optimizer = optim.Adam(value.parameters(), lr=args.vlr)
-    last_a = [-1, -1, -1]
+    # create model
+    model = GeeseNet()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    # load model
+    # load weights
     if args.load != '':
-        policy.load_state_dict(torch.load(args.load, device))
+        model.load_state_dict(torch.load(args.load, map_location=device))
+
+    model.to(device)
 
     # for log
     tag = ','.join([f'{arg}={getattr(args, arg)}'
                     for arg in vars(args) if arg != 'load'])
-    tb = SummaryWriter(comment=tag)
     running_reward, running_steps = 0, 0
-    entropies = deque(maxlen=args.step_log_interval)
-    values = deque(maxlen=args.step_log_interval)
-    actions = deque(maxlen=args.step_log_interval)
-    logits = deque(maxlen=args.step_log_interval)
-    action2int = {'NORTH': 0, 'EAST': 1, 'WEST': 2, 'SOUTH': 3}
 
-    # for self-play
-    prev_policies = deque(maxlen=args.prev_pi)
-    prev_policies.append(policy.state_dict())
-    oppo_index = 0
+    # for tensorboard
+    tb = SummaryWriter(comment=tag)
 
-    for i_episode in range(1, 200001):
-        # self-play start
-        if i_episode == args.start_self:
-            for i in range(3):
-                random_index = random.randint(0, len(prev_policies) - 1)
-                oppo_policy[i].load_state_dict(prev_policies[random_index])
-            trainer = env.train([None, opponent, opponent, opponent])
+    for i_episode in range(1, args.n_epi):
+        obs, ep_reward = trainer.reset(), 0
 
-        observation, ep_reward = trainer.reset(), 0
-        process_reward.prev_alive = 3
-
-        for step in range(1, 201):
-            action, obs = agent(observation, configuration, train=True)
-            v = value(obs)
-            observation, reward, done, info = trainer.step(action)
-            reward = process_reward(observation, reward, done)
-            policy.saved_rewards.append(reward)
-            value.saved_values.append(v)
+        for step in range(1, 200):
+            action = agent(obs, None, train=True)
+            obs, reward, done, info = trainer.step(action)
+            reward = process_reward(obs, done)
+            model.saved_rewards.append(reward)
             ep_reward += reward
-            actions.append(action2int[action])
             if done:
                 break
 
         running_reward = running_reward * 0.99 + ep_reward * 0.01
         running_steps = running_steps * 0.99 + step * 0.01
-        entropies += deque(torch.cat(policy.saved_entropies).tolist())
-        values += deque(torch.cat(value.saved_values).tolist())
-        logits += deque(torch.cat(policy.saved_logits).reshape(-1))
 
-        finish_episode(i_episode)
+        finish_episode()
 
         if i_episode % args.log_interval == 0:
-            print(f'Episode {i_episode}\t'
-                  f'Average reward: {running_reward:.2f}\t'
-                  f'Average steps: {running_steps:.2f}\t')
+            print(f'Episode {i_episode} \t'
+                  f'Average reward: {running_reward:.2f} \t'
+                  f'Average steps: {running_steps:.2f} \t')
             tb.add_scalar('reward', running_reward, i_episode)
             tb.add_scalar('steps', running_steps, i_episode)
-            tb.add_histogram('actions', np.array(actions), i_episode,
-                             bins=7)
-            tb.add_histogram('entropies', np.array(entropies), i_episode)
-            tb.add_histogram('values', np.array(values), i_episode)
-            tb.add_histogram('logits', np.array(logits), i_episode)
-            torch.save(policy.state_dict(), f'models/policy_{tag}.pt')
-
-        if i_episode > args.start_self \
-                and i_episode % args.change_interval == 0:
-            prev_policies.append(deepcopy(policy.state_dict()))
-            random_index = random.randint(0, len(prev_policies) - 1)
-            oppo_policy[oppo_index].load_state_dict(
-                prev_policies[random_index])
-            oppo_index = (oppo_index + 1) % 3
+            torch.save(model.state_dict(), f'models/{tag}.pt')
 
     tb.close()
