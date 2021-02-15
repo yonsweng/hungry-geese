79b980b55242c6bfabe685f6d7327fc71208dca7
yonsweng <yonsweng@gmail.com>
2021-02-15 23:43:23 +0900

    Adjust n_envs

--- stable_baseline.py
+++ stable_baseline.py
@@ -4,10 +4,11 @@ from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3.common.env_util import make_vec_env
 from callback import CustomCallback
 
-env = make_vec_env(HungryGeeseEnv, n_envs=16)
+num_cpu = 4
+env = make_vec_env(HungryGeeseEnv, n_envs=num_cpu)
 
 model = PPO(MlpPolicy, env, verbose=1, tensorboard_log='ppo_logs')
 callback = CustomCallback(
-    save_freq=16000, save_path='ppo_models', name_prefix='model'
+    save_freq=8000, save_path='ppo_models', name_prefix='model'
 )  # save_freq should be divided by n_envs
-model.learn(8000000, callback=callback)
+model.learn(4000000, callback=callback)
